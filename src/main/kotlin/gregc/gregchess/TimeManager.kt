df0b281c5ea889ab86b06cd200d197e9d346299a
Gregory1234 <Gregory1234@users.noreply.github.com>
2021-05-29 15:11:42 +0200

    Made TimeManager functions inline

--- src/main/kotlin/gregc/gregchess/TimeManager.kt
+++ src/main/kotlin/gregc/gregchess/TimeManager.kt
@@ -1,22 +1,25 @@
 package gregc.gregchess
 
-import org.bukkit.Bukkit
 import org.bukkit.scheduler.BukkitRunnable
 import java.time.Duration
 
 object TimeManager {
 
-    class CancellableContext internal constructor(private val r: BukkitRunnable) {
+    class CancellableContext constructor(private val r: BukkitRunnable) {
         fun cancel() {
             r.cancel()
         }
     }
 
-    fun runTaskLater(delay: Duration, callback: () -> Unit) {
-        Bukkit.getScheduler().runTaskLater(GregInfo.plugin, Runnable(callback), delay.toTicks())
+    inline fun runTaskLater(delay: Duration, crossinline callback: () -> Unit) {
+        object : BukkitRunnable() {
+            override fun run() {
+                callback()
+            }
+        }.runTaskLater(GregInfo.plugin, delay.toTicks())
     }
 
-    fun runTaskTimer(delay: Duration, period: Duration, callback: CancellableContext.() -> Unit) {
+    inline fun runTaskTimer(delay: Duration, period: Duration, crossinline callback: CancellableContext.() -> Unit) {
         object : BukkitRunnable() {
             val cc = CancellableContext(this)
             override fun run() {
