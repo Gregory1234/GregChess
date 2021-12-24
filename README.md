0f259acf6b4fe0ad38d710bffddbb1130676b3ff
Gregory1234 <Gregory1234@users.noreply.github.com>
2021-12-24 21:36:33 +0100

    Cleaned up the custom gradle tasks

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # GregChess
 A Bukkit plugin and a WIP Fabricmc mod adding chess to minecraft written in Kotlin.
 
-Works in Minecraft `1.17.1`.
+Works in Minecraft `1.18.1`.
 
 ## Build
 To build the bukkit jar use gradle task `:createSpigotJar`.
@@ -9,3 +9,5 @@ To build the bukkit jar use gradle task `:createSpigotJar`.
 To build the fabric jar use gradle task `:createFabricJar`.
 
 The jars are moved to the root project directory.
+
+To run the fabric client use gradle task `:runFabricClient`.
--- build.gradle.kts
+++ build.gradle.kts
@@ -15,32 +15,30 @@ tasks {
         it.outputs.upToDateWhen { false }
     }
 
-    create<DefaultTask>("createSpigotJar") {
+    create<Copy>("createSpigotJar") {
         group = "gregchess"
         dependsOn(":gregchess-core:test")
         dependsOn(":gregchess-bukkit:jar")
-        doLast {
-            copy {
-                from(project(":gregchess-bukkit").getTasksByName("jar", true))
-                into(rootDir)
-                rename {
-                    "${rootProject.name}-$version-bukkit.jar"
-                }
-            }
-        }
+        getByPath(":gregchess-bukkit:compileKotlin").mustRunAfter(":gregchess-core:test")
+        getByPath(":bukkit-utils:compileKotlin").mustRunAfter(":gregchess-core:test")
+        from(getByPath(":gregchess-bukkit:jar"))
+        into(rootDir)
+        rename { "${rootProject.name}-$version-bukkit.jar" }
     }
-    create<DefaultTask>("createFabricJar") {
+    create<Copy>("createFabricJar") {
         group = "gregchess"
         dependsOn(":gregchess-core:test")
         dependsOn(":gregchess-fabric:remapJar")
-        doLast {
-            copy {
-                from(project(":gregchess-fabric").getTasksByName("remapJar", true))
-                into(rootDir)
-                rename {
-                    "${rootProject.name}-$version-fabric.jar"
-                }
-            }
-        }
+        getByPath(":gregchess-fabric:compileKotlin").mustRunAfter(":gregchess-core:test")
+        from(getByPath(":gregchess-fabric:remapJar"))
+        into(rootDir)
+        rename { "${rootProject.name}-$version-fabric.jar" }
     }
+    create<DefaultTask>("runFabricClient") {
+        group = "gregchess"
+        dependsOn(":gregchess-core:test")
+        dependsOn(":gregchess-fabric:runClient")
+        getByPath(":gregchess-fabric:compileKotlin").mustRunAfter(":gregchess-core:test")
+    }
+    // TODO: add a way of running a spigot server with the plugin from a gradle task
 }
