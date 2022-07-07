aa3752b3e0cf411b7f4d2dbc796dd9888f164406
Gregory1234 <gregory1234@users.noreply.github.com>
2022-07-07 21:54:27 +0200

    Updated the dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,9 @@ dependencies {
     val kotlinVersion: String by props
     implementation(kotlin("gradle-plugin", kotlinVersion))
     val dokkaVersion: String by props
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:$dokkaVersion")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:$dokkaVersion") {
+        exclude("org.jetbrains.kotlin","kotlin-stdlib-jdk8") // https://github.com/Kotlin/dokka/issues/2546
+    }
 }
 
 tasks {
