21ab5def8fc5368b8002a2464557a5bcfbb4604a
Gregory1234 <gregory1234@users.noreply.github.com>
2022-08-28 01:16:59 +0200

    Made chess workbench work

--- fabric/src/main/kotlin/gregc/gregchess/fabric/client/ChessWorkbenchScreen.kt
+++ fabric/src/main/kotlin/gregc/gregchess/fabric/client/ChessWorkbenchScreen.kt
@@ -1,25 +1,33 @@
 package gregc.gregchess.fabric.client
 
-import gregc.gregchess.fabric.DefaultedListInventory
-import gregc.gregchess.fabric.GregChessMod
+import gregc.gregchess.fabric.*
+import gregc.gregchess.fabric.piece.item
+import gregc.gregchess.piece.*
 import io.github.cottonmc.cotton.gui.SyncedGuiDescription
 import io.github.cottonmc.cotton.gui.ValidatedSlot
+import io.github.cottonmc.cotton.gui.client.BackgroundPainter
 import io.github.cottonmc.cotton.gui.client.CottonInventoryScreen
-import io.github.cottonmc.cotton.gui.widget.WGridPanel
-import io.github.cottonmc.cotton.gui.widget.WItemSlot
+import io.github.cottonmc.cotton.gui.networking.NetworkSide
+import io.github.cottonmc.cotton.gui.networking.ScreenNetworking
+import io.github.cottonmc.cotton.gui.widget.*
 import io.github.cottonmc.cotton.gui.widget.data.Insets
+import io.github.cottonmc.cotton.gui.widget.icon.ItemIcon
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
-import net.minecraft.item.Items
 import net.minecraft.recipe.RecipeUnlocker
 import net.minecraft.screen.ScreenHandlerContext
+import net.minecraft.tag.TagKey
 import net.minecraft.text.Text
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
+import java.util.function.Predicate
 import kotlin.math.min
+import gregc.gregchess.registry.Registry as GregChessRegistry
 
 class WChessWorkbenchResultSlot(
-    inventory: Inventory, val input: Inventory, val player: PlayerEntity?, index: Int
+    inventory: Inventory, val input: Inventory, val player: PlayerEntity?, index: Int, val justCrafted: (Boolean) -> Unit
 ) : WItemSlot(inventory, index, 1, 1, true) {
 
     override fun isInsertingAllowed(): Boolean = false
@@ -59,11 +67,19 @@ class WChessWorkbenchResultSlot(
 
             override fun onTakeItem(player: PlayerEntity, stack: ItemStack) {
                 this.onCrafted(stack)
+                justCrafted(true)
                 for (i in 0..1) {
                     input.removeStack(i, 1)
                 }
+                justCrafted(false)
             }
         }
+    }
+}
+
+class WGridListPanel : WGridPanel() {
+    fun clear() {
+        children.clear()
     }
 }
 
@@ -73,32 +89,101 @@ class ChessWorkbenchGuiDescription(
     GregChessMod.CHESS_WORKBENCH_SCREEN_HANDLER_TYPE, syncId, playerInventory,
     null, getBlockPropertyDelegate(context, 0)
 ) {
+    private val bases = TagKey.of(Registry.ITEM_KEY, ident("piece_base"))
+    private val whiteDyes = TagKey.of(Registry.ITEM_KEY, Identifier("c", "dye_white"))
+    private val blackDyes = TagKey.of(Registry.ITEM_KEY, Identifier("c", "dye_black"))
 
     private val input = DefaultedListInventory(2, ::onInputChanged)
     private val output = DefaultedListInventory(1)
 
+    private val recipeList = mutableListOf<Piece>()
+    private var selected: Piece? = null
+
+    private val recipeListPanel: WGridListPanel
+
+    private var justCrafted = false
+
 
     init {
-        val root = WGridPanel()
+        // TODO: add scrolling in case of too many items
+        // TODO: style the buttons better
+        val root = WGridPanel(9)
         setRootPanel(root)
         root.insets = Insets.ROOT_PANEL
 
-        root.add(WChessWorkbenchResultSlot(output, input, playerInventory?.player, 0), 6, 2)
-        root.add(WItemSlot(input, 0, 1, 1, false), 1, 2)
-        root.add(WItemSlot(input, 1, 1, 1, false), 3, 2)
+        root.add(WChessWorkbenchResultSlot(output, input, playerInventory?.player, 0) { justCrafted = it }, 15, 3)
+        root.add(WItemSlot(input, 0, 1, 1, false).apply { filter = Predicate { it.isIn(bases) } }, 1, 1)
+        root.add(WItemSlot(input, 1, 1, 1, false).apply { filter = Predicate { it.isIn(whiteDyes) || it.isIn(blackDyes) } }, 1, 5)
 
-        root.add(this.createPlayerInventoryPanel(), 0, 4)
+        recipeListPanel = WGridListPanel()
+
+        recipeListPanel.backgroundPainter = BackgroundPainter.createColorful(0xff51493a.toInt())
+
+        root.add(recipeListPanel, 4, 1, 9, 6)
+
+        root.add(this.createPlayerInventoryPanel(), 0, 8)
+
+        ScreenNetworking.of(this, NetworkSide.SERVER).receive(ident("select")) {
+            val s = it.readString()
+            for (p in recipeList) {
+                if (p.id.toString() == s) {
+                    selected = p
+                    output.setStack(0, p.item.defaultStack)
+                    break
+                }
+            }
+        }
+        ScreenNetworking.of(this, NetworkSide.SERVER).receive(ident("unselect")) {
+            println("unselect")
+            output.clear()
+            selected = null
+        }
 
         root.validate(this)
     }
 
-    private fun onInputChanged() {
-        if (input.getStack(0).isItemEqual(ItemStack(Items.BEDROCK))
-            && input.getStack(1).isItemEqual(ItemStack(Items.BEDROCK))) {
-            output.setStack(0, ItemStack(Items.BEDROCK))
-        } else {
-            output.removeStack(0)
+    private fun readdRecipes() {
+        recipeListPanel.clear()
+        for ((y, row) in recipeList.chunked(4).withIndex()) {
+            for ((x, p) in row.withIndex()) {
+                val button = WButton(ItemIcon(p.item))
+                button.onClick = Runnable {
+                    output.setStack(0, p.item.defaultStack)
+                    selected = p
+                    ScreenNetworking.of(this, NetworkSide.CLIENT).send(ident("select")) {
+                        it.writeString(p.id.toString())
+                    }
+                }
+                recipeListPanel.add(button, x, y, 1, 1)
+            }
         }
+        if (selected !in recipeList) {
+            selected = null
+            if (!output.isEmpty && !justCrafted) {
+                output.clear()
+                ScreenNetworking.of(this, NetworkSide.CLIENT).send(ident("unselect")) {}
+            }
+        } else if (selected != null) {
+            output.setStack(0, selected!!.item.defaultStack)
+            ScreenNetworking.of(this, NetworkSide.CLIENT).send(ident("select")) {
+                it.writeString(selected!!.id.toString())
+            }
+        }
+        recipeListPanel.validate(this)
+    }
+
+    private fun onInputChanged() {
+
+        recipeList.clear()
+
+        if (input.getStack(0).isIn(bases) && input.getStack(1).isIn(whiteDyes)) {
+            recipeList.addAll(GregChessRegistry.PIECE_TYPE.values.map(::white))
+        }
+        if (input.getStack(0).isIn(bases) && input.getStack(1).isIn(blackDyes)) {
+            recipeList.addAll(GregChessRegistry.PIECE_TYPE.values.map(::black))
+        }
+
+        readdRecipes()
     }
 
     override fun close(player: PlayerEntity?) {
