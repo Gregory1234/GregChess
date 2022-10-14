72377d3ef74d65eb6935d0213cd418897065eccf
Gregory1234 <gregory1234@users.noreply.github.com>
2022-10-14 21:47:33 +0200

    Fixed dokka

--- buildSrc/src/main/kotlin/Dokka.kt
+++ buildSrc/src/main/kotlin/Dokka.kt
@@ -1,15 +1,17 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 import org.gradle.api.Project
 import org.jetbrains.dokka.gradle.GradleDokkaSourceSetBuilder
 import java.net.URL
 
-fun GradleDokkaSourceSetBuilder.gregchessSourceLink(project: Project) = sourceLink {
+inline fun GradleDokkaSourceSetBuilder.gregchessSourceLink(project: Project) = sourceLink {
     val relPath = project.rootProject.projectDir.toPath().relativize(project.projectDir.toPath())
     localDirectory.set(project.projectDir.resolve("src"))
     remoteUrl.set(URL("https://github.com/Gregory1234/GregChess/tree/master/$relPath/src"))
     remoteLineSuffix.set("#L")
 }
 
-fun GradleDokkaSourceSetBuilder.externalDocumentationLinkElementList(url: String) = externalDocumentationLink {
+inline fun GradleDokkaSourceSetBuilder.externalDocumentationLinkElementList(url: String) = externalDocumentationLink {
     this.url.set(URL(url))
     this.packageListUrl.set(URL(url + "element-list"))
 }
